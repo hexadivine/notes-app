79ab3150c1056476e8dabb4806f52ccbb90655ca
hexadivine <hexadivine@gmail.com>
2024-01-28 00:34:52 +0530

    created sidebar to list all notes

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BrandInfo from "./BrandInfo/BrandInfo";
+
+function Sidebar() {
+    return (
+        <div className="w-[20%] h-screen bg-dark-blue flow-root">
+            <BrandInfo />
+        </div>
+    );
+}
+
+export default Sidebar;
