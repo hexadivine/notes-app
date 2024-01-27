013d3df957fcec5de18f2c8e0a79df2d36437e14
hexadivine <hexadivine@gmail.com>
2024-01-28 00:35:10 +0530

    added brandpage to sidebar

---
+++ src/components/Sidebar/BrandInfo/BrandInfo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BrandInfo() {
+    return (
+        <div className="bg-transparent p-5 text-center text-[20px] font-bold m-3 mt-5 rounded-lg ">
+            Notes App
+        </div>
+    );
+}
+
+export default BrandInfo;
