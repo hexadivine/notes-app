9cbe7dd44793fba6f8755d65f316bd159b1468f8
hexadivine <hexadivine@gmail.com>
2024-01-28 00:36:01 +0530

    integrated sidebar and texteditor

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Sidebar from "./components/Sidebar/Sidebar";
+import TextEditor from "./components/TextEditor/TextEditor";
+
 function App() {
-    return <div className=""> 
-    
-    </div>;
+    return (
+        <div className="flex">
+            <Sidebar />
+            <TextEditor />
+        </div>
+    );
 }
 
 export default App;
