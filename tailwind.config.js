8fd928310d89e5b7a1fd2203ca8b9c8299b9c6d2
hexadivine <hexadivine@gmail.com>
2024-01-28 00:33:43 +0530

    added required color variables to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {},
+        colors: {
+            'dark-blue' : '#1a1f29',
+            'light-blue' : '#1f2430',
+            'transparent' : 'rgba(255, 255, 255, 0.1)'
+        }
     },
     plugins: [],
 };
