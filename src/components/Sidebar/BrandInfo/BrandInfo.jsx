b94f786ef244a61a982f085a4fd79da1965b294c
hexadivine <hexadivine@gmail.com>
2024-01-31 22:33:36 +0530

    changed brand name

--- src/components/Sidebar/BrandInfo/BrandInfo.jsx
+++ src/components/Sidebar/BrandInfo/BrandInfo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function BrandInfo() {
     return (
         <div className="bg-transparent p-5 text-center text-[20px] font-bold m-3 mt-5 rounded-lg ">
-            Notes App
+            Notes++
         </div>
     );
 }
