ac1f7fbdbc68e591072f092638e30f70b5f7c2e3
hexadivine <hexadivine@gmail.com>
2024-01-31 22:33:50 +0530

    added text editor

--- src/components/TextEditor/TextEditor.jsx
+++ src/components/TextEditor/TextEditor.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function TextEditor() {
     return <div className="w-[80%] h-screen bg-light-blue">
-        <input type="text" className="w-full py-10 bg-light-blue font-bold text-[30px] px-5 focus:outline-none font-mono" placeholder="Title"/>
+        <input type="text" className="w-full py-10 bg-light-blue font-bold text-[30px] px-5 focus:outline-none font-['Poppins']" style={{forntFamily: 'poppins'}} placeholder="Title"/>
         <textarea className="w-full h-[calc(100vh-131px)] bg-light-blue py-10 px-5 focus:outline-none font-mono" placeholder="Content"></textarea>
     </div>;
 }
